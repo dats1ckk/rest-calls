9d00b374f50a32938430bc12d29879bafbacf5db
Dan Baciu <danbaciu@Dans-iMac.local>
2025-05-21 12:13:12 +0300

    Initial commit

---
+++ src/test/java/xyz/afbet/calls/RestCallsApplicationTests.java
@@ -0,0 +1,13 @@
+package xyz.afbet.calls;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class RestCallsApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
